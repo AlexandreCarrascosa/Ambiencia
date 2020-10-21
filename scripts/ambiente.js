806f3f95bbba90071293ea6dd3750462b01b8efa
AlexandreProjeto <alexandrecarrascosa@gmail.com>
2020-10-21 10:22:39 -0400

    News

--- scripts/ambiente.js
+++ scripts/ambiente.js
@@ -1,3 +1,20 @@
-let colorinfo = document.querySelector(div.history);
+var myTab = document.getElementById('myTable');
 
-console.log(colorInfo)
+for (i = 1; i < myTab.rows.length; i++) {
+
+    // GET THE CELLS COLLECTION OF THE CURRENT ROW.
+    var objCells = myTab.rows.item(i).cells;
+
+    // LOOP THROUGH EACH CELL OF THE CURENT ROW TO READ CELL VALUES.
+    for (var j = 0; j < objCells.length; j++) {
+        
+        if(objCells.item(j).innerHTML == "OFF"){
+            objCells.item(j).style.color = "red"
+        }
+
+        if(objCells.item(j).innerHTML == "ON"){
+            objCells.item(j).style.color = 'lightgreen'
+        }
+    }
+
+}
