cdc26be34da2f82aaf76a7ecc4ee2f534ae12a2e
AlexandreCarrascosa <56851746+AlexandreCarrascosa@users.noreply.github.com>
2020-11-28 16:41:56 -0400

    'Update'

--- backend/update.py
+++ backend/update.py
@@ -1,16 +1,22 @@
 #Mudar para a pasta do site C:\Users\alexa\Documents\Ambience\site\
-import subprocess as sp
+from subprocess import check_output, Popen, PIPE, STDOUT
 import os
 
 def Atualizar():
 #Muda diretório para site
-    os.chdir(r'..//Site')
+    os.chdir(r'..//')
 
     #Salvar todos os gits
-    save = sp.check_output(["git", "add", "."])
-    send = sp.check_output(["git", "commit", "-m", "'Update'"])
-    up = sp.check_output(["git", "push"])
+    save = check_output(["git", "add", "."])
+    send = check_output(["git", "commit", "-m", "'Update'"])
+    #up = check_output(["git", "push"])
+    
+    p = Popen(["git", "push"], stdout=PIPE, stdin=PIPE, stderr=STDOUT, universal_newlines=True)
+    
+    info = ["alexandrecarrascosa", "725e171b8bdba60628c2d779b5ae1bcc6cdd1b9f"]
+    
+    output, err = p.communicate(input=f'{info[0]}\n{info[1]}\n')
 
-    return save, send, up
+    return save, send, output, err
 
 Atualizar()
