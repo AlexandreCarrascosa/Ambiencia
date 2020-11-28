16cbacd0e6dbf7ed23b91c5ff2b2c02195cbeae0
AlexandreCarrascosa <56851746+AlexandreCarrascosa@users.noreply.github.com>
2020-11-28 16:43:11 -0400

    'Update'

--- backend/update.py
+++ backend/update.py
@@ -16,6 +16,7 @@ def Atualizar():
     info = ["alexandrecarrascosa", "725e171b8bdba60628c2d779b5ae1bcc6cdd1b9f"]
     
     output, err = p.communicate(input=f'{info[0]}\n{info[1]}\n')
+    print(output)
 
     return save, send, output, err
 
