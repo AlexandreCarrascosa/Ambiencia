b6777c6f8e85c34a30dc9d16f010bba94a504700
AlexandreCarrascosa <56851746+AlexandreCarrascosa@users.noreply.github.com>
2020-11-28 17:45:28 -0400

    'Update'

--- backend/update.py
+++ backend/update.py
@@ -15,7 +15,7 @@ def Atualizar():
     info = ["alexandrecarrascosa", "725e171b8bdba60628c2d779b5ae1bcc6cdd1b9f"]
     #p = Popen(["git", "push"], stdout=PIPE, stdin=PIPE, universal_newlines=True)
     
-    up = check_output(["git", "push", f"{info[0]}", f"{info[1]}"])
+    up = check_output(["git", "push"])
 
     #output = Popen.communicate(f'{info[0]}\n{info[1]}\n')
     
