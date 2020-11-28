2cac70cfd70e8d85bb6b1bf827a64fda8406679b
AlexandreCarrascosa <56851746+AlexandreCarrascosa@users.noreply.github.com>
2020-11-28 16:47:01 -0400

    'Update'

--- backend/update.py
+++ backend/update.py
@@ -15,7 +15,7 @@ def Atualizar():
     sleep(2)
     p = Popen(["git", "push"], stdout=PIPE, stdin=PIPE, stderr=STDOUT, universal_newlines=True)
     
-    spleep(1)
+    sleep(1)
     info = ["alexandrecarrascosa", "725e171b8bdba60628c2d779b5ae1bcc6cdd1b9f"]
     
     output, err = p.communicate(input=f'{info[0]}\n{info[1]}\n')
