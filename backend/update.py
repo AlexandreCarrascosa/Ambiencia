b0b960aae2a733785bb53587e0f87bfc0b641d16
AlexandreCarrascosa <56851746+AlexandreCarrascosa@users.noreply.github.com>
2020-11-28 17:18:47 -0400

    'Update'

--- backend/update.py
+++ backend/update.py
@@ -10,16 +10,18 @@ def Atualizar():
     #Salvar todos os gits
     save = check_output(["git", "add", "."])
     send = check_output(["git", "commit", "-m", "'Update'"])
-    #up = check_output(["git", "push"])
+    
     
     info = [b"alexandrecarrascosa", b"725e171b8bdba60628c2d779b5ae1bcc6cdd1b9f"]
-    p = Popen(["git", "push"], stdout=PIPE, stdin=PIPE, universal_newlines=True)
-    output = p.communicate(input=f'{info[0]}\n{info[1]}\n')
+    #p = Popen(["git", "push"], stdout=PIPE, stdin=PIPE, universal_newlines=True)
+    
+    up = check_output(["git", "push", info[0], info[1]])
+
     #output = Popen.communicate(f'{info[0]}\n{info[1]}\n')
     
    
    
-    return save, send, output
+    return save, send, up
 
 Atualizar()
 
