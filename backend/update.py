21fa682a9000e8a49b52871c1ea43d7553a61d0d
AlexandreCarrascosa <56851746+AlexandreCarrascosa@users.noreply.github.com>
2020-11-28 18:08:46 -0400

    'Update'

--- backend/update.py
+++ backend/update.py
@@ -4,6 +4,13 @@ import os
 from time import sleep
 
 
+def Timer(init, end):
+	while init != end:
+		print(f"A atualização será feita em {end} seg. \nTempo decorrido: {init} s")
+		init += 1 
+		sleep(1)
+		os.system('clear')
+
 def Atualizar():
 #Muda diretório para site
     print(os.chdir(r'..//'))
@@ -12,17 +19,14 @@ def Atualizar():
     save = check_output(["git", "add", "."])
     send = check_output(["git", "commit", "-m", "'Update'"])
     up = check_output(["git", "push"])
+    
+    Timer(0,300)
 
     return save, send, up
     
-def Timer(init, end):
-	while init != end:
-		print(f"A atualização será feita em {end} seg. \nTempo decorrido: {init} s")
-		init += 1 
-		sleep(1)
-		os.system('clear')
+
 
 while True:
 	Atualizar()
-	sleep((60*5))
+	sleep(300)
 
