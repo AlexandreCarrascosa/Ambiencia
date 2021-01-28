e663e3f42df24bf82de1dee363b1bd27a145e1cd
AlexandreProjeto <alexandrecarrascosa@gmail.com>
2021-01-28 14:18:31 -0400

    'Update'

--- backend/readTemp.py
+++ backend/readTemp.py
@@ -11,74 +11,76 @@ from CalcPsic import CalcPsic
 from sys import platform
 
 if platform == 'win32':
-	conexao = serial.Serial('COM4', 9600)
+        conexao = serial.Serial('COM4', 9600)
 else:
-	conexao = serial.Serial('/dev/ttyACM0', 9600)
+        conexao = serial.Serial('/dev/ttyACM0', 9600)
 
 path = '../data.xml'
 tree = ET.parse(path)
 root = tree.getroot()
 
 def readTemp():
-	while True:
-		read = conexao.readline().strip()
-		valor = str(read)
-		terminal = valor.split("'")[1]
-		
-		return terminal
-	
+        while True:
+                read = conexao.readline().strip()
+                valor = str(read)
+                terminal = valor.split("'")[1]
+                
+                return terminal
+        
 def Data():
-	atual = datetime.now()
-	data = atual.strftime("%d/%m/20%y")
-	hora = atual.strftime("%H:%M")
-	
-	return data, hora
-	
-	
+        atual = datetime.now()
+        data = atual.strftime("%d/%m/20%y")
+        hora = atual.strftime("%H:%M")
+        
+        return data, hora
+        
+        
 
 while True:
-	msg = "Dados escritos!\nIniciando contagem para próximo registro:"
-	Timer(0, 10, msg)
-	#print(f'{Data()[0]}, {Data()[1]}, {readTemp()}')
-	umidade = readTemp()[0:5]
-	temperatura = readTemp()[6:]
-	
-	info = ET.SubElement(root, 'Info')
-	
-	data = ET.SubElement(info, 'data')
-	data.text = Data()[0]
-	
-	hora = ET.SubElement(info, 'hora')
-	hora.text = Data()[1]
-	
-	temp = ET.SubElement(info, 'temp')
-	temp.text = temperatura
-	
-	umid = ET.SubElement(info, 'umd')
-	umid.text = umidade
-	
-	aspr = ET.SubElement(info, 'aspr')
-	aspr.text = 'OFF'
+        msg = "Dados escritos!\nIniciando contagem para próximo registro:"
+        Timer(0, 10, msg)
+        #print(f'{Data()[0]}, {Data()[1]}, {readTemp()}')
+        umidade = readTemp()[0:5]
+        temperatura = readTemp()[6:10]
 
-	vent = ET.SubElement(info, 'vent')
-	vent.text = 'OFF'
-
-	lamp = ET.SubElement(info, 'lamp')
-	lamp.text = 'OFF'
-
-	tree.write('../data.xml')
-		
-	AtualRefresh(Data()[0],
-		     Data()[1],
-		     temperatura,
-	 	     umidade)
-	 	     
-	CalcPsic(temperatura, umidade)
+        print(readTemp())
         
-	sleep(10)
-	Atualizar()
-	
-	
-	#print(f'{Data()[0]}, {Data()[1]}, {umidade}, {temperatura}')
-	
-	
+        info = ET.SubElement(root, 'Info')
+        
+        data = ET.SubElement(info, 'data')
+        data.text = Data()[0]
+        
+        hora = ET.SubElement(info, 'hora')
+        hora.text = Data()[1]
+        
+        temp = ET.SubElement(info, 'temp')
+        temp.text = temperatura
+        
+        umid = ET.SubElement(info, 'umd')
+        umid.text = umidade
+        
+        aspr = ET.SubElement(info, 'aspr')
+        aspr.text = 'OFF'
+
+        vent = ET.SubElement(info, 'vent')
+        vent.text = 'OFF'
+
+        lamp = ET.SubElement(info, 'lamp')
+        lamp.text = 'OFF'
+
+        tree.write('../data.xml')
+
+        AtualRefresh(Data()[0],
+                     Data()[1],
+                     temperatura,
+                     umidade)
+                     
+        CalcPsic(temperatura, umidade)
+        
+        sleep(10)
+        Atualizar()
+        
+
+        if temperatura > 32:
+                kit.sendwhatmsg("+5566999087650", "Temperatura acima de 32ºC, verifique a Estufa!")
+
--- scripts/info.js
+++ scripts/info.js
@@ -1,2 +1,40 @@
+var pvs = document.getElementById("pvs")
+var ppv = document.getElementById("ppv")
+var rms = document.getElementById("rms")
+var rm = document.getElementById("rm")
+var tpo = document.getElementById("tpo")
+var v = document.getElementById("v")
+var p = document.getElementById("p")
+var h = document.getElementById("h")
+var tbu = document.getElementById("tbu")
+
 xmlhttp = new XMLHttpRequest();
-xmlhttp.open("GET", "../CalcAtual.xml", false)
+xmlhttp.open("GET", "../calc.xml", false);
+xmlhttp.send();
+xmlDoc = xmlhttp.responseXML;
+
+var x = xmlDoc.getElementsByTagName("root");
+
+for (i=0;i<x.length;i++)
+    {
+        pvs.innerHTML = x[i].getElementsByTagName("PVS")[0].childNodes[0].nodeValue;
+        document.write("<tr><td>");
+        document.write("</td><td>");
+        ppv.innerHTML = x[i].getElementsByTagName("PPV")[0].childNodes[0].nodeValue;
+        document.write("</td><td>");
+        rms.innerHTML = x[i].getElementsByTagName("RMS")[0].childNodes[0].nodeValue;
+        document.write("</td><td>");
+        rm.innerHTML =(x[i].getElementsByTagName("RM")[0].childNodes[0].nodeValue);
+        document.write("</td><td>");
+        tpo.innerHTML =(x[i].getElementsByTagName("TPO")[0].childNodes[0].nodeValue);
+        document.write("</td><td>");
+        v.innerHTML =(x[i].getElementsByTagName("v")[0].childNodes[0].nodeValue);
+        document.write("</td><td>");
+        p.innerHTML =(x[i].getElementsByTagName("p")[0].childNodes[0].nodeValue);
+        document.write("</td><td>");
+        h.innerHTML =(x[i].getElementsByTagName("h")[0].childNodes[0].nodeValue);
+        document.write("</td><td>");
+        tbu.innerHTML =(x[i].getElementsByTagName("TBU")[0].childNodes[0].nodeValue);
+        document.write("</td></tr>");
+    }
+document.write('</table></div></div></section>');
